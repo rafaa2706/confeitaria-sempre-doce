ca46d69b1376fa4846414e753042cc3f89b25f40
rafaa2706 <166833162+rafaa2706@users.noreply.github.com>
2024-07-26 16:16:24 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def inicial():
     return render_template("index.html")
 
+@app.route("/contatos")
+def contato():
+    return render_template("contato.html")
+
+@app.route("/cardapio")
+def cardapio():
+    return render_template("cardapio.html")
+
 if __name__ == '__main__':
     app.run()
